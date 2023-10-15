c866a735a657b7aad8accec559ab068c6b7709b4
Cristian <r.cristiano.ai@gmail.com>
2023-10-15 18:51:07 -0400

    Added Item.js as new file in model; modified styles.css, script.js, and server.js.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Item = require('./models/Item')
 
 const app = express();
 
@@ -27,7 +28,50 @@ app.use(express.json())
 //Set EJS as templating engine
 app.set('view engine', 'ejs')
 
-//LOGIC GOES HERE
+// Routes
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+
+app.get('/item', async (req, res) => {
+  const items = await Item.find({});
+  res.render('item', { items });
+});
+
+// Create
+app.post('/item', async (req, res) => {
+  const newItem = new Item(req.body);
+  try {
+    await newItem.save();
+    res.redirect('/item');
+  } catch (err) {
+    //you can set up custom error handling on the client side using this information
+    res.redirect('/item?error=true');
+  }
+});
+
+// Update
+app.post('/item/update/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  try {
+    await Item.findByIdAndUpdate(id, { name, description });
+    res.redirect('/item');
+  } catch (err) {
+    //you can set up custom error handling on the client side using this information
+    res.redirect(`/item?error=true`);
+  }
+});
+
+// Delete
+app.delete('/item/delete/:id', async (req, res) => {
+  const { id } = req.params;
+  await Item.findByIdAndDelete(id);
+  //Send success back to the client-side function
+  res.status(200).json({ message: 'Item deleted successfully' });
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on: http://localhost:${port}`)
