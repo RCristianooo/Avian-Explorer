0ecd0f0c018a2f1e93fe669560b839d924d6f221
Cristian <r.cristiano.ai@gmail.com>
2023-10-15 17:11:23 -0400

    Added views and public folders; css, img, and js folders inside public folder; partials folder inside views folder; added various files for Avian Explorer App. Finally, a few bugs were squashed!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = expesss ()
+const app = express();
 
 const port = process.env.PORT || 4000
 
